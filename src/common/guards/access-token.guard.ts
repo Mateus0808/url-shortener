606b4e84df4594f6ad6b6ae5b6af260aac8c50b5
Mateus dos Santos Loiola <mateus.santos.016@ufrn.edu.br>
2025-06-07 18:31:58 -0300

    fix: add option access token guard

--- src/common/guards/access-token.guard.ts
+++ src/common/guards/access-token.guard.ts
@@ -3,3 +3,13 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class AccessTokenGuard extends AuthGuard('jwt') {}
+
+@Injectable()
+export class OptionalAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: any) {
+    if (err || info) {
+      console.warn('Auth info:', info);
+    }
+    return user || null;
+  }
+}
