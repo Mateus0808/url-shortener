84df96e71eeb3a81cb5ceb547d071f272a1b53c4
Mateus dos Santos Loiola <mateus.santos.016@ufrn.edu.br>
2025-06-07 18:26:06 -0300

    feat: add auth strategies

---
+++ src/features/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IGetUserByParamService, IGetUserByParamServiceToken } from 'src/features/user/interfaces/services/get-user-by-param.interface';
+
+type JwtPayload = { 
+  id: string;
+  name: string;
+  email: string;
+};
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    @Inject(IGetUserByParamServiceToken)
+    private readonly userService: IGetUserByParamService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_ACCESS_SECRET ?? '',
+    })
+  }
+
+  async validate(payload: JwtPayload): Promise<JwtPayload> {
+    return { id: payload.id, email: payload.email, name: payload.name }
+  }
+}
